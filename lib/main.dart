0b18f07c37eaf1f7562f3daf746aa6a269c5265b
Dewmal <dewmal@syigen.com>
2021-12-14 11:50:12 +0530

    Mobx intergration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/store/counter.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+final Counter counter = Counter();
 
 void main() {
   runApp(const MyApp());
@@ -15,53 +19,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'CeylonAI'),
+      home: const MyPageWithMobx(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyPageWithMobx extends StatelessWidget {
+  const MyPageWithMobx({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Ceylon AI"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Count with Mobx',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Observer(
+                builder: (_) => Text(
+                      '${counter.value}',
+                      style: Theme.of(context).textTheme.headline4,
+                    )),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => {counter.increment()},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+    ;
   }
 }
